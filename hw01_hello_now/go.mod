a9af2e3e86b5f13dc36af7cab1ae6eee6244b7ff
AGrishin63 <grishin2@yandex.ru>
2020-03-26 19:25:55 +0300

    HW1 is completed

--- hw01_hello_now/go.mod
+++ hw01_hello_now/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw01_hello_now
+module github.com/AGrishin63/griHW/hw01_hello_now
 
 go 1.14
 
--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"time"
+
+	ntp "github.com/beevik/ntp"
+)
+
 func main() {
+	fmt.Println("current time:\t", time.Now().Format("2006-01-02T15:04:05.999999-07:00"))
+
+	//t.err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	t, err := ntp.Time("0.beevik-nool.ntp.org")
+
+	if err != nil {
+		os.Stderr.WriteString("Error on getting exact time!")
+		//fmt.Println("Error on getting exact time!")
+		return
+	}
+
+	fmt.Println("exact time:\t", t.Format("2006-01-02T15:04:05.999999-07:00"))
 	// Place your code here
 }
