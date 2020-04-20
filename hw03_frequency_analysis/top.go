e9ec57f7f81cbc6b777cbefcb7c432033830c7cd
AGrishin63 <grishin2@yandex.ru>
2020-04-20 19:05:39 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,37 +1,13 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
-//package main
 
 import (
 	"fmt"
-	//	"os"
 
 	s "strings"
 )
 
-// func main() {
-// 	top := Top10("dd dd  ff  gg  gg gg jj  jj    d s e d c v g f h t r r r r r  mjh mjh mjh mjh mjh kljhn lk lk lk ryf ryf ryf ryf\nryf ryf ryf ryf ryf ryf ryf ryf ryf ryf i p yu 5t 5t 5t 5t 5t e e e e e e e")
-// 	if top == nil {
-// 		os.Stderr.WriteString("Error on getting exact time!")
-// 		return
-// 	}
-// 	fmt.Println(top)
-// 	for i := 0; i < len(top); i++ {
-// 		fmt.Printf("%s %s", i, top[i])
-// 	}
-// 	return
-//}
 const top int = 10
 
-// func swapS(one *string, two *string) {
-// 	tmp := *one
-// 	*one = *two
-// 	*two = tmp
-// }
-// func swapI(one *int, two *int) {
-// 	tmp := *one
-// 	*one = *two
-// 	*two = tmp
-// }
 func Top10(text string) []string {
 	// Place your code here
 	text = s.Replace(text, "\n", " ", -1)
@@ -63,8 +39,6 @@ func Top10(text string) []string {
 					tmp2 := word
 					word = topKey[i]
 					topKey[i] = tmp2
-					//swapI(&v, &topVal[i])
-					//swapS(&word, &topKey[i])
 				} //if
 			} //for
 			if i < top {
