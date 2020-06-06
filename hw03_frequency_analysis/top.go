9dcf6493a106531d4ee7f0cd97500bfc369d13d2
AGrishin63 <grishin2@yandex.ru>
2020-06-06 10:17:05 +0300

    HW3 is completed 0606

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,54 +1,46 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
 
 import (
-	"fmt"
-
-	s "strings"
+	"sort"
+	"strings"
 )
 
 const top int = 10
 
 func Top10(text string) []string {
 	// Place your code here
-	text = s.Replace(text, "\n", " ", -1)
-	words := s.Split(text, " ")
+	text = strings.Replace(text, "\n", " ", -1)
+	words := strings.Split(text, " ")
 	dict := make(map[string]int)
-	var topVal []int = make([]int, 0, top)
-	var topKey []string = make([]string, 0, top)
-	var topMin int = 0
-	//Заполнить частотный словарь.
-	fmt.Println(text)
-	for j := 0; j < len(words); j++ {
-		word := s.Trim(words[j], " \t")
-		if word != "" {
-			v := dict[word]
-			v++
-			dict[word] = v
-		} //if
-	} //for
-	//выбрать топ 10 упорядоченных по убыванию
-	for word, v := range dict {
-		if v > topMin {
-			//занести в тор
-			var i int
-			for i = 0; i < len(topVal); i++ {
-				if v > topVal[i] {
-					tmp1 := v
-					v = topVal[i]
-					topVal[i] = tmp1
-					tmp2 := word
-					word = topKey[i]
-					topKey[i] = tmp2
-				} //if
-			} //for
-			if i < top {
-				topVal = append(topVal, v)
-				topKey = append(topKey, word)
-			} else {
-				topMin = v
-			} //if
-		} //if
-	} //for
+	topKey := make([]string, 0, top)
 
+	//Заполнить частотный словарь.
+	for j := 0; j < len(words); j++ {
+		word := strings.Trim(words[j], " \t")
+		if word != "" {
+			dict[word]++
+		}
+	}
+
+	//создать слайс структур
+	type item struct {
+		wrd string
+		frq int
+	}
+	tmp := make([]item, 0, len(dict))
+
+	//перелить словарь в слайс структур
+	for word, v := range dict {
+		t := new(item)
+		t.wrd = word
+		t.frq = v
+		tmp = append(tmp, *t)
+	}
+
+	// сортировать и взять первые 10
+	sort.Slice(tmp, func(i, j int) bool { return tmp[i].frq > tmp[j].frq })
+	for m := 0; m < top && m < len(tmp); m++ {
+		topKey = append(topKey, tmp[m].wrd)
+	}
 	return topKey
 }
